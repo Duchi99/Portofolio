8dea4975031d545c55b2e4bbed9907021260deb5
Admin <admin@MacBookPro.home>
2024-10-06 18:06:32 +0100

    side nav bar working

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,15 @@
+<script setup>
+import SideNav from '@/Components/SideNav.vue';
+import { ref } from 'vue'
+
+const isCollapsed = ref(true);
+const toggleSidebar = () => {
+    isCollapsed.value = !isCollapsed.value
+}
+</script>
+
+<template>
+    <div>
+        <SideNav :isCollapsed="isCollapsed" @toggleSidebar="toggleSidebar"/>
+    </div>
+</template>
